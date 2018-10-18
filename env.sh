7ad6bcf8574670976117fc09abc54c5adeee787d
jsmont <jsmont.sol@gmail.com>
2018-10-18 09:27:00 +0200

    Env script created, now downloading the docker image and aliasing verilator

---
+++ env.sh
@@ -0,0 +1,16 @@
+export REPOROOT="$( cd "$(dirname "$0")"; pwd -P)"
+
+if [ -z ${OLD_PATH+x} ]; then
+    export OLD_PATH=$PATH;
+else
+    export PATH=$OLD_PATH;
+fi
+
+export PATH="$REPOROOT/tools/scripts:$PATH"
+export RTLROOT="$REPOROOT/src"
+export DOCKERTAG="jsola/verilator:latest"
+if [[ "$(docker images -q $DOCKERTAG 2> /dev/null)" == "" ]]; then
+    docker pull $DOCKERTAG
+fi
+
+alias verilator='docker run -w $REPOROOT -v $REPOROOT:$REPOROOT --name=tmp_container --rm $DOCKERTAG bash verilator'
