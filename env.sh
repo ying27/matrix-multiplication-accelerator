bda899aee4847c23bfc67aa76329f3661453d589
jsmont <jsmont.sol@gmail.com>
2018-11-10 12:07:04 +0100

    Changes to environment and first attempts to create a generic testing framework

--- env.sh
+++ env.sh
@@ -1,18 +1,21 @@
-export REPOROOT="$( cd "$(dirname "$0")"; pwd -P)"
+#Detect the path to the top of the repo from which we are calling the script
+export REPOROOT=$( cd $(dirname "$0"); pwd -P)
 
+#Add our scripts to the path
 if [ -z ${OLD_PATH+x} ]; then
     export OLD_PATH=$PATH;
 else
     export PATH=$OLD_PATH;
 fi
-
 export PATH="$REPOROOT/tools/scripts:$PATH"
+
+#Specify the path f the rtl and testbenches
 export RTLROOT="$REPOROOT/src"
 export TBROOT="$REPOROOT/tb"
+
+#Pull docker image if necessary
 export DOCKERTAG="jsola/verilator:latest"
 if [[ "$(docker images -q $DOCKERTAG 2> /dev/null)" == "" ]]; then
     docker pull $DOCKERTAG
 fi
 
-#alias verilator='docker run -w $REPOROOT -v $REPOROOT:$REPOROOT --name=tmp_container --rm $DOCKERTAG bash verilator'
-alias cleanup='for i in `find $REPOROOT -name build`; do ( cd $i/..;  rm -r build ); done'
